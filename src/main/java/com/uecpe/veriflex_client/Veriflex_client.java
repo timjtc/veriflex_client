67cda1d6b16ae452ef986008ca5bd903f30db2cf
Timothy James Concepcion <concepciontimothy@gmail.com>
2023-12-05 04:57:40 +0800

    first commit

---
+++ src/main/java/com/uecpe/veriflex_client/Veriflex_client.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uecpe.veriflex_client;
+
+/**
+ *
+ * @author user0
+ */
+public class Veriflex_client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
